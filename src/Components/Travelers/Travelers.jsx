b1537232a6a898cbc2dae543fcfd5e33ee32d4cc
Rezwanulrimel <rezwanulrimel@gmail.com>
2024-03-28 19:37:14 +0600

    travelers information added

--- src/Components/Travelers/Travelers.jsx
+++ src/Components/Travelers/Travelers.jsx
@@ -2,9 +2,39 @@ import React from "react";
 
 //! imported desctination images
 import Dhaka from "../../assets/Dhaka2.avif";
+import Coxsbazar from "../../assets/coxsbazar.jpg";
+// import khulna from "../../assets/khulna.jpg";
 
 // Import traveler images
 import traveler1 from "../../assets/traveler.jpg";
+import traveler2 from "../../assets/traveler.jpg";
+// import traveler3 from "../../assets/traveler.jpg";
+
+// Maping
+const travelers = [
+  {
+    id: 1,
+    destinationImg: Dhaka,
+    travelerImg: traveler2,
+    travelerName: "Dihan",
+    socialLink: "@Dihan97",
+  },
+  {
+    id: 2,
+    destinationImg: Coxsbazar,
+    travelerImg: traveler1,
+    travelerName: "Rimel",
+    socialLink: "@Rimel97",
+  },
+  // {
+  //   id: 3,
+  //   destinationImg: khulna,
+  //   travelerImg: traveler3,
+  //   travelerName: "Farhan",
+  //   socialLink: "@Farhan95",
+  // },
+];
+
 const Travelers = () => {
   return (
     <div className="travelers container section">
@@ -12,20 +42,28 @@ const Travelers = () => {
         <h2>Top travelers of this months!</h2>
 
         <div className="tavelersContainer grid">
-          {/*  Single Traveler Card  */}
-          <div className="sinlgeTraveler">
-            {/* Destination */}
-            <img src={Dhaka} className="destinationImg" />
-            {/* Traveler Details */}
-            <div className="travelerDetails">
-              <div className="travelerPicture">
-                <img src={traveler1} className="travelerImg" />
-              </div>
-              <div className="travelerName">
-                <span>Dihan</span>
-              </div>
-            </div>
-          </div>
+          {/* maping  */}
+          {travelers.map(
+            ({ id, destinationImg, travelerImg, travelerName, socialLink }) => {
+              return (
+                // {/*  Single Traveler Card  */}
+                <div key={id} className="sinlgeTraveler">
+                  {/* Destination */}
+                  <img src={destinationImg} className="destinationImg" />
+                  {/* Traveler Details */}
+                  <div className="travelerDetails">
+                    <div className="travelerPicture">
+                      <img src={travelerImg} className="travelerImg" />
+                    </div>
+                    <div className="travelerName">
+                      <span>{travelerName}</span>
+                      <p> {socialLink} </p>
+                    </div>
+                  </div>
+                </div>
+              );
+            }
+          )}
         </div>
       </div>
     </div>
